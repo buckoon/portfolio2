7c35c6bd2c0ad66e0ab0cb03eb74cae9ea991163
jerm <buckoon99@gmail.com>
2023-02-10 12:41:01 -0500

    Building backend

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gid2l38z',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/social.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'social',
+  title: 'Social',
+  type: 'document',
+  fields: [
+    {
+      name: "title",
+      title:"title",
+      description:"platform for social media",
+      type:"string",
+    },
+    
+    
+    {
+      name: "url",
+      title:"Url",
+      type:"url",
+      
+      
+    },
+    
+
+
+
+
+  
+
+  ],
+}
