2c6ee5503cf43d9655bcc5430f9c138e5df8dcc3
jerm <buckoon99@gmail.com>
2023-02-13 14:01:30 -0500

    still working on backend

---
+++ sanity-stuff/schemas/project.ts
@@ -0,0 +1,50 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Title of the Project',
+      type: 'string',
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Summary',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'linkToBuild',
+      title: 'LinkToBuild',
+      type: 'url',
+    }),
+    defineField({
+      name: 'technologies',
+      title: 'Technologies',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'skill'}}],
+    }),
+    defineField({
+      name: 'dateStarted',
+      title: 'Date Started',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'dateEnded',
+      title: 'Date Ended',
+      type: 'datetime',
+    }),
+
+  ],
+})
