2e650cfd2a6fd8b265e78d924c0be76995f7a389
jerm <buckoon99@gmail.com>
2023-02-13 20:37:15 -0500

    before I try updating tscongig.json

--- sanity-stuff/sanity.config.ts
+++ sanity-stuff/sanity.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './sanity-stuff/schemas'
+import {schemaTypes} from './schemas'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
 
 export default defineConfig({
   name: 'Jeremy',
